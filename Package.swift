6605aa0e701a26393f4b30e2bea05f68937f1a77
William Dillon <william@housedillon.com>
2016-02-18 16:09:53 -0800

    WIP Starting to support swift package manager

---
+++ Package.swift
@@ -0,0 +1,11 @@
+//
+//  Package.swift
+//  PMJSON
+//
+//  Created by William Dillon on 2/18/16.
+//  Copyright © 2016 Postmates. All rights reserved.
+//
+
+import PackageDescription
+
+let package = Package(name: "PMJSON")
